cf153d7fa7e6a504d1673258ad4af35a69f38f83
Nurasyl <nurasil9704@gmail.com>
2023-10-30 02:02:26 +0500

    first commit

---
+++ app/src/main/java/kz/course/movieapplication/MainActivity.kt
@@ -0,0 +1,34 @@
+package kz.course.movieapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.aboutItem -> {
+                true
+            }
+
+            R.id.settingsItem-> {
+                true
+            }
+
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
+    }
+}
