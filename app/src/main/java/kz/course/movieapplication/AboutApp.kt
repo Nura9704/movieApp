812588f8274d2425a9f9d00f22ae5c9fb2433275
Nurasyl <nurasil9704@gmail.com>
2023-10-30 03:14:48 +0500

    lesson10

---
+++ app/src/main/java/kz/course/movieapplication/AboutApp.kt
@@ -0,0 +1,11 @@
+package kz.course.movieapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AboutApp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_app)
+    }
+}
